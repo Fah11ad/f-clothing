a407716f59a61774b4641d4e4226b9951dca5cbe
Fah11ad <falqurashi11@gmail.com>
2020-05-15 14:42:56 +0300

    broken our homepage to smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+
+const Homeage = () => (
+  <div className="homeage">
+  <Directory />
+      </div>
+);
+
+export default Homeage;
