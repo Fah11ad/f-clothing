11a0d6327755bc64357b73851d929331c2253ad4
Fah11ad <falqurashi11@gmail.com>
2020-05-23 02:10:45 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utls.js
@@ -0,0 +1,16 @@
+// Utitlity function allow us to keep ourfiles clean and organixeand we may use it in multiple files in one location
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+};
+
